48f9cb0cbfe12af0d1e042636f3b77cf6950bc83
jaem-in-park <165642908+jaem-in-park@users.noreply.github.com>
2024-09-12 15:38:21 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,12 @@
 $(function () {
   // 슬라이드
-  let slideInterval;
   function slide() {
-    $(".slide ul").animate({ left: "-100%" }, 800, function () {
+    $(".slide ul").animate({ left: "-100%" }, 1000, function () {
       $(".slide ul").append($(".slide ul li").first());
       $(".slide ul").css({ left: "0" });
     });
   }
-  // 슬라이드를 시작하는 함수
-  function startSlide() {
-    slideInterval = setInterval(slide, 2000);
-  }
-  // 슬라이드를 멈추는 함수
-  function stopSlide() {
-    clearInterval(slideInterval);
-  }
-  // 페이지 로드 시 슬라이드를 시작
-  startSlide();
-
-  // 슬라이드 영역에 마우스를 올리면 멈추고
-  $(".slide").on("mouseenter", stopSlide);
-
-  // 슬라이드 영역에서 마우스를 떼면 다시 시작
-  $(".slide").on("mouseleave", startSlide);
+  setInterval(slide, 3000);
 
   // 카테고리 메뉴
   $(".header-list > a , .Ab>li>a").mouseenter(function () {
